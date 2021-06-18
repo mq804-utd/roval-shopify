66324b8ad028b32e1324b3db9996c47838c559e2
mq804-utd <mq804.utd@gmail.com>
2021-06-18 14:52:45 +0300

    Ported

--- src/scripts/components/langConvertSelect.js
+++ src/scripts/components/langConvertSelect.js
@@ -1,10 +1,13 @@
 const basicSingle = '.js-lang_select-single'
 function formatState (state) {
     if (!state.id) return state.text
-    const baseUrl = "icons/flags";
-    return $('<span><img src="' + baseUrl + '/' + state.element.value.toLowerCase() + '.png" class="img-flag" style="width: 31px"/> ' + state.text + '</span>')
+	console.log(state.element)
+    return $('<span><img src="' + state.element.value.toLowerCase() + '.png" class="img-flag" style="width: 31px"/> ' + state.text + '</span>')
 }
+
+console.log('here')
 $(document).ready(function() {
+	console.log('here')
     $(basicSingle).select2({
         width: '50px',
         templateResult: formatState,
